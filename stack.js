8e47dcc97072a181095905f63bf6e6de1f4abd7b
Daniel Fong <daniel.fong.mv@gmail.com>
2020-04-03 13:10:08 -0600

    good enough on stacks

--- stack.js
+++ stack.js
@@ -42,20 +42,22 @@ function createEmpty() {
 function main() {
     let starTrek = createStarTrek();
     let empty = createEmpty();
-    peek(starTrek);
-    isEmpty(starTrek);
-    isEmpty(empty);
-    starTrek.pop(); // remove Scotty
-    starTrek.pop(); // remove MoCoy
-    starTrek.push('Scotty'); // add Scotty back
-    display(starTrek);
+    let numStack = createNumStack();
+    // peek(starTrek);
+    // isEmpty(starTrek);
+    // isEmpty(empty);
+    // starTrek.pop(); // remove Scotty
+    // starTrek.pop(); // remove MoCoy
+    // starTrek.push('Scotty'); // add Scotty back
+    // display(starTrek);
+    //sortStack(numStack);
 }
 
 main();
 
 
 function peek(stack){
-  console.log('top', stack.top);
+  return stack.top;
 }
 
 function isEmpty(stack) {
@@ -95,20 +97,101 @@ function is_palindrome(s) {
     console.log(stack1.top.data)
    
     let palindrome;
-    while(stack1.top !== null) {
-        if(stack1.top.data === stack2.top.data) {
-            stack1.pop();
-            stack2.pop();
-            palindrome = 'True'
-        }
-        palindrome = 'False'
-    }
+    // while(stack1.top !== null) {
+    //     if(stack1.top.data === stack2.top.data) {
+    //         stack1.pop();
+    //         stack2.pop();
+    //         palindrome = 'True'
+    //     }
+    //     palindrome = 'False'
+    // }
     return console.log(palindrome);
     // stack1 = a q f q a
     // stack2 = a q f q a
 }
 
-console.log(is_palindrome("dad"));
-console.log(is_palindrome("A man, a plan, a canal: Panama"));
-console.log(is_palindrome("1001"));
-console.log(is_palindrome("Tauhida"));
+// console.log(is_palindrome("dad"));
+// console.log(is_palindrome("A man, a plan, a canal: Panama"));
+// console.log(is_palindrome("1001"));
+// console.log(is_palindrome("Tauhida"));
+
+function matchParens(exp) {
+    // 0
+    // (())
+    // +1 for left paren (push), -1 for right paren(pop)
+    // if 0 at end = all matching
+    // if -1 or +1 its wrong 
+    // if its not ( or ) then dont put in the stack 
+    // if NULL is too early, then it is missing a right paren
+
+    let stack1 = new Stack();
+
+    // (((45 + 34) * 5))
+    // 
+    for(let i=0; i<exp.length; i++){
+        let char = exp.slice(i, i+1);
+        if (char === '(') {
+        stack1.push(char);
+        }
+        else if(char === ')' && stack1.top) {
+            stack1.pop()
+        }
+        else if(char === ')' && !stack1.top) {
+            return console.log('you done missed a (')
+        }
+    }
+    if(stack1.top) {
+        return console.log('you done missed a )')
+    }
+    return console.log('you done did a good job')
+}
+
+matchParens(')')
+
+function sortStack(stack){
+    if(!stack.top) {
+        return console.log('stack is empty')
+    }
+  let tempStack = new Stack();
+  // get the first value
+  if(tempStack.top === null) {
+    tempStack.push(stack.pop());
+  }
+  let switched = false;
+  while (stack.top !== null) {
+      let topTemp = peek(tempStack);
+      let topStack = stack.pop();
+      if(topTemp <= topStack) {
+          tempStack.push(topStack);
+      }
+      stack.push(tempStack.pop());
+      tempStack.push(topStack);
+      switched = true;
+  }
+
+//    3
+//    2
+//    1
+  if(switched = true) {
+      sortStack(tempStack);
+  }
+
+  return console.log(stack)
+  
+}
+
+function createNumStack () {
+    let numStack = new Stack();
+    numStack.push(1);
+    numStack.push(7);
+    //
+    // numStack.push(10);
+    // numStack.push(12);
+    // numStack.push(3);
+    // numStack.push(16);
+    // numStack.push(3);
+    return numStack;
+}
+
+
+
